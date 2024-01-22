40f1b384647eefc88c57209a823240c08da21220
MinaMagdyLuis <minaluis2017@gmail.com>
2024-01-22 16:13:16 +0200

    +shared_prefernces

--- islamic/lib/main.dart
+++ islamic/lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:islamic/chapter_details_screen/chapter_details_screen.dart';
+import 'package:islamic/data/shared_helper.dart';
 import 'package:islamic/hadeth_details/hadeth_details_screen.dart';
 import 'package:islamic/home_screen.dart';
 import 'package:islamic/myThemeData.dart';
 import 'package:islamic/provider/settings_provider.dart';
 import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SharedPreferencesHelper.prefs = await SharedPreferences.getInstance();
   runApp(
     ChangeNotifierProvider<SettingsProvider>(
       create: (BuildContext context) {
-        return SettingsProvider();
+        return SettingsProvider()..init();
       },
       child: const MyApp(),
     ),
@@ -26,7 +30,7 @@ class MyApp extends StatelessWidget {
     SettingsProvider settingsProvider = Provider.of<SettingsProvider>(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Islamic',
+      title: "Islami",
       theme: MyThemeData.lightTheme,
       darkTheme: MyThemeData.darkTheme,
       themeMode: settingsProvider.currentTheme,
--- islamic/lib/provider/settings_provider.dart
+++ islamic/lib/provider/settings_provider.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:islamic/data/shared_helper.dart';
 
 class SettingsProvider extends ChangeNotifier {
   ThemeMode currentTheme = ThemeMode.light;
   String currentLanguage = 'en';
 
+  init() {
+    String newLang = SharedPreferencesHelper.getLanguage();
+    changeLanguage(newLang);
+    bool isLightMode = SharedPreferencesHelper.getMode();
+    if (isLightMode) {
+      changeTheme(ThemeMode.light);
+    } else {
+      changeTheme(ThemeMode.dark);
+    }
+  }
+
   void changeTheme(ThemeMode mode) {
     if (mode == currentTheme) return;
     currentTheme = mode;
+    if (mode == ThemeMode.light) {
+      SharedPreferencesHelper.setMode(true);
+    } else {
+      SharedPreferencesHelper.setMode(false);
+    }
+
     notifyListeners();
   }
 
@@ -23,6 +41,8 @@ class SettingsProvider extends ChangeNotifier {
   void changeLanguage(String language) {
     if (currentLanguage == language) return;
     currentLanguage = language;
+    SharedPreferencesHelper.setLanguage(language);
+
     notifyListeners();
   }
 
